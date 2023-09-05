943c1cf503b7c3944709eb057baf24c51cd56cc4
tommyHellraiser <nacho.ponce25@gmail.com>
2023-09-05 02:53:01 -0300

    Started adding logger functions and basic structs

--- src/logger/the_logger.rs
+++ src/logger/the_logger.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+use lazy_static::lazy_static;
+use tokio::sync::RwLock;
 
 //  TODO: determine if it's possible to receive either a string message or a TheError struct inside a macro
 #[derive(Default)]
@@ -11,3 +14,85 @@ pub enum LogLevel {
     Debug,
     Trace
 }
+
+lazy_static!(
+    static ref THE_LOGGER: TheLogger = TheLogger::new();
+);
+
+pub struct TheLogger {
+    config: RwLock<TheLoggerConfig>
+}
+
+#[derive(Default)]
+struct TheLoggerConfig {
+
+}
+
+impl TheLogger {
+    pub fn new() -> Self {
+        Self {
+            config: RwLock::new(TheLoggerConfig::default())
+        }
+    }
+
+    pub fn hide_date() {
+
+    }
+
+    pub fn hide_years() {
+
+    }
+
+    pub fn hide_months() {
+
+    }
+
+    pub fn hide_days() {
+
+    }
+
+    pub fn hide_timestamp() {
+
+    }
+
+    pub fn timestamp_format() {
+
+    }
+
+    pub fn hide_hours() {
+
+    }
+
+    pub fn hide_minutes() {
+
+    }
+
+    pub fn hide_secs() {
+
+    }
+
+    pub fn hide_millisecs() {
+
+    }
+
+    pub fn hide_level() {
+
+    }
+
+    pub fn level_with_square_brackets() {
+
+    }
+
+    pub fn hide_error_line() {
+
+    }
+
+    pub fn hide_error_column() {
+
+    }
+}
+
+
+/*
+YYYY-MM-DD HH:MM:SS.mm LEVEL \t @ FILE LINE:COL -> Error itself
+ */
