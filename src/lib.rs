f83c84b79a7fb9c43ad04886056c67eb3672e33c
tponcePrex <tponce@prexcard.com.ar>
2023-09-13 17:36:10 -0300

    Removed logger and datetime modules from crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ extern crate core;
 
 mod macros;
 mod errors;
-mod logger;
 
 pub use errors::the_error::{TheResult, TheError, TheErrorType};
 pub use errors::system_error_codes::SystemErrorCodes;
-pub use logger::the_logger::{TheLogger, TheLoggerConfig};
 
--- src/logger/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod the_logger;
-
